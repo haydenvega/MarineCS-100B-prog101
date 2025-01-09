1072946711b1150f2eaf0593fb8dbf3f6ca2692b
haydenvega <vegahayden33@gmail.com>
2025-01-08 22:51:25 -0800

    done

--- exercises/prog101-expressions-variables.R
+++ exercises/prog101-expressions-variables.R
@@ -3,13 +3,13 @@ temp_F <- temp_C * 9/5 - 32
 rect_area <- rect_length * rect_width
 
 # Fill in the question marks to complete these expressions
-????_area <- 1/2 * base * height
-cylinder_volume <- pi * ????^2 * ???
+triangle_area <- 1/2 * base * height
+cylinder_volume <- pi * radius^2 * height
 
   # What are the errors in these expressions?
-  speed_m_s <- distance_m * time_s
-acceleration m s2 <- speed_m_s / time_s
-force_N < mass + acceleration_m_s2
+  speed_m_s <- distance_m * time_s #divide
+acceleration m s2 <- speed_m_s / time_s # to be a smart ass, I don't think this works
+force_N < mass + acceleration_m_s2 #multiply
 
 # The energy density of market squid is 4850 joules per gram[1]. If a 225 kg
 # Risso's dolphin needs 5 million joules of energy per day[2] and a typical
@@ -17,7 +17,13 @@ force_N < mass + acceleration_m_s2
 # eat each day? Solve the above word problem in R. Consider how many variables
 # you need to create, what to name them to make the code readable, and how to
 # combine them into expressions.
+energy_squid <- 4850
+weight_squid <- 35
+energy_need <- 5000000
 
+answer <- energy_need/energy_squid/weight_squid
+
+answer
 # [1] Price et al. 2024 https://doi.org/10.3389/fmars.2023.1345525
 # [2] Barlow et al. 2008 https://doi.org/10.3354/meps07695
 
